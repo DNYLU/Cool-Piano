ecee3d525e3e580ab1b74e7eb275bf0cb7b4ece1
DNY <dannylu1999@gmail.com>
2022-10-28 16:52:29 +0200

    Slettet kode der ikke skulle bruges

--- piano/app.js
+++ piano/app.js
@@ -1,67 +1,41 @@
 // JavaScript Drum Kit App
-
 {
+	// Dette burde ikke være nødvendigt, men knapperne virker ikke hvis den ikke er her
 	const playingClass = 'playing',
 		crashRide = document.getElementById('crash-ride'),
 		hiHatTop = document.getElementById('hihat-top');
 
-	const animateCrashOrRide = () => {
-		crashRide.style.transform = 'rotate(0deg) scale(1.5)';
-	};
-
-	const animateHiHatClosed = () => {
-		hiHatTop.style.top = '171px';
-	};
-
 	const playSound = e => {
 		const keyCode = e.keyCode,
+			// Viser hvilken knap der er blevet trykket på
 			keyElement = document.querySelector(`div[data-key="${keyCode}"]`);
 
-		if(!keyElement) return;
+		if (!keyElement) return;
 
+		// Viser hvilken lyd der skal afspilles
 		const audioElement = document.querySelector(`audio[data-key="${keyCode}"]`);
 		audioElement.currentTime = 0;
 		audioElement.play();
 
-		switch(keyCode) {
-			case 69:
-			case 82:
-				animateCrashOrRide();
-				break;
-			case 75:
-				animateHiHatClosed();
-				break;
-		}
-
 		keyElement.classList.add(playingClass);
 	};
 
-	const removeCrashRideTransition = e => {
-		if(e.propertyName !== 'transform') return;
 
-		e.target.style.transform = 'rotate(-7.2deg) scale(1.5)';
-	};
-
-	const removeHiHatTopTransition = e => {
-		if(e.propertyName !== 'top') return;
-
-		e.target.style.top = '166px';
-	};	
 
 	const removeKeyTransition = e => {
-		if(e.propertyName !== 'transform') return;
+		if (e.propertyName !== 'transform') return;
 
 		e.target.classList.remove(playingClass)
 	};
 
-	const drumKeys = Array.from(document.querySelectorAll('.key'));
+	// Laver en array af alle divs med classen key
+	const pianoKeys = Array.from(document.querySelectorAll('.key'));
 
-	drumKeys.forEach(key => {
+	//Når en transition er færdig, fjern klassen playing
+	pianoKeys.forEach(key => {
 		key.addEventListener('transitionend', removeKeyTransition);
 	});
 
-	crashRide.addEventListener('transitionend', removeCrashRideTransition);
-	hiHatTop.addEventListener('transitionend', removeHiHatTopTransition);
-
+	// Når der trykkes på en tast, kald funktionen playSound
 	window.addEventListener('keydown', playSound);
 }
